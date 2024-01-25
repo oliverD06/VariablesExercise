cd9c60bd7e6bc2fb4e06600494af37ef4b2d626b
Oliver Delaney <oliver.delaney@stu.barren.kyschools.us>
2024-01-25 10:04:20 -0600

    Exercise and bonus completed

---
+++ script.js
@@ -0,0 +1,16 @@
+// const
+let num = 20
+num = 15
+num = num + 7
+num--
+
+console.log(num)
+num = "This is not a number"
+let blank = null
+console.log(blank)
+blank = ""
+//bonus
+const quarter = 0.25, dime = 0.10, nickel = 0.05, penny = 0.01
+console.log(quarter, dime, nickel, penny)
+let letters = "qwertyuiop"
+console.log(letters.length, letters[9])
